b3eb35e8c75e3a7af01a75862fa5ad0be868e736
Catalin Ciobanu <caciobanu@gmail.com>
2019-11-29 16:09:48 +0200

    Updated meta

--- src/auto-commit-backend.ts
+++ src/auto-commit-backend.ts
@@ -1,43 +1,33 @@
-
-/**
- * Generated using theia-plugin-generator
- */
-
 import * as theia from '@theia/plugin';
 
 export async function start(context: theia.PluginContext) {
     let gitLogHandlerInitialized: boolean;
 
     const onChange = () => {
-        theia.window.showInformationMessage('Hello World 1!');
-
         // Get the vscode Git plugin if the plugin is started.
         const gitExtension = theia.plugins.getPlugin('vscode.git');
         if (!gitLogHandlerInitialized && gitExtension && gitExtension.exports) {
-            theia.window.showInformationMessage('Hello World 2!');
-
             gitLogHandlerInitialized = true;
 
             const git: any = gitExtension.exports._model.git;
 
             const listener = async (out: string) => {
-                theia.window.showInformationMessage('Hello World 3!');
                 console.log("----------------------");
                 console.log(out);
             }
 
             git.onOutput.addListener('log', listener);
 
-            theia.workspace.onDidSaveTextDocument((elem) => {
-                theia.window.showInformationMessage('Hello World 4!');
-
+            theia.workspace.onDidSaveTextDocument((elem: any) => {
                 let elems = elem.fileName.split('/');
                 elems.pop();
                 let dir = elems.join('/');
 
-                git.exec(dir, ['add', elem.fileName]).then(() => {}).catch(() => {});
-                git.exec(dir, ["commit", "-am", "test"]).then(() => {}).catch(() => {});
-                git.exec(dir, ['push']).then(() => {}).catch(() => {});
+                git.exec(dir, ['add', elem.fileName]).then(() => {
+                    git.exec(dir, ["commit", "-am", "test"]).then(() => {
+                        git.exec(dir, ['push']).then(() => {}).catch(() => {});
+                    }).catch(() => {});
+                }).catch(() => {});
             });
 
         }
