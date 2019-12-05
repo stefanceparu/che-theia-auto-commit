cf6a842916676725646789087561145f50de73d9
stefan ceparu <stefan.ceparu@hackajob.co>
2019-12-05 15:01:05 +0200

    update

--- src/auto-commit-backend.ts
+++ src/auto-commit-backend.ts
@@ -1,10 +1,9 @@
-
-/**
- * Generated using theia-plugin-generator
- */
-
 import * as theia from '@theia/plugin';
-import * as che from '@eclipse-che/plugin';
+
+let modifiedGitDirs: string[] = [];
+let commitLock: boolean = false;
+let timeoutInterval: number = 5000; // miliseconds
+let interval: any = null;
 
 export async function start(context: theia.PluginContext) {
     let gitLogHandlerInitialized: boolean;
@@ -15,31 +14,65 @@ export async function start(context: theia.PluginContext) {
         if (!gitLogHandlerInitialized && gitExtension && gitExtension.exports) {
             gitLogHandlerInitialized = true;
 
-            const git: any = gitExtension.exports._model.git;
-
-            const listener = async (out: string) => {
-                console.log("----------------------");
-                console.log(out);
+            if (typeof theia.workspace.rootPath === undefined) {
+                return;
             }
+            let rootPath = String(theia.workspace.rootPath);
 
-            git.onOutput.addListener('log', listener);
+            const workspacePath = new theia.RelativePattern(rootPath, "**/*");
+            const fsw = theia.workspace.createFileSystemWatcher(workspacePath);
 
-            theia.workspace.onDidSaveTextDocument((elem) => {
-                let elems = elem.fileName.split('/');
-                elems.pop();
-                let dir = elems.join('/');
+            const git = gitExtension.exports._model.git;
 
-                git.exec(dir, ['add', elem.fileName]);
-                git.exec(dir, ["commit", "-am", "test"]);
-                git.exec(dir, ['push']);
-            });
+            const fswListener = function(file: any) {
+                fileCommit(git, file.path);
+            };
 
+            fsw.onDidCreate(fswListener);
+            fsw.onDidChange(fswListener);
+            fsw.onDidDelete(fswListener);
+
+            if (interval === null) {
+                interval = setInterval(function() { commitChanges(git); }, timeoutInterval);
+            }
         }
     }
 
     theia.plugins.onDidChange(onChange);
 }
 
-export function stop() {
+function fileCommit(git: any, filePath: string): void {
+    let elems = filePath.split('/');
+    elems.pop();
+    let dir = elems.join('/');
 
+    git.exec(dir, ['rev-parse', '--show-toplevel']).then(
+        (elem: any) => {
+            if (elem.stdout && modifiedGitDirs.indexOf(elem.stdout.trim()) === -1) {
+                modifiedGitDirs.push(elem.stdout.trim());
+            }
+        }
+    ).catch(() => { });
+}
+
+function commitChanges(git: any): void {
+    if (commitLock && modifiedGitDirs.length == 0) {
+        return;
+    }
+    commitLock = true;
+
+    modifiedGitDirs.forEach((dir) => {
+        git.exec(dir, ['add', '--all']).then(() => {
+            git.exec(dir, ['commit', '-am', 'auto']).then(() => {
+                git.exec(dir, ['push']).then(() => { commitLock = false; }).catch(() => { commitLock = false; });
+            }).catch(() => { commitLock = false; });
+        }).catch(() => { commitLock = false; });
+    });
+}
+
+export function stop() {
+    clearInterval(interval);
+    modifiedGitDirs = [];
+    commitLock = false;
+    interval = null;
 }
